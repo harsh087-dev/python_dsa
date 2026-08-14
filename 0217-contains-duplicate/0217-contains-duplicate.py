fc99d4aeb0ed6967e10d80e63c95bc98ec9df3c2
Harsh Gond <harshgond087@gmail.com>
2026-08-14 21:37:52 +0530

    Time: 16 ms (50.08%), Space: 37.3 MB (6.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -2,10 +2,8 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         dict={}
         for i in nums:
-           
+            
             if i in dict:
                 return True
-                break
-            dict[i]=dict.get(i,0)+1    
-
+            dict[i]=1    
         return False        
